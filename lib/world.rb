f267c78820a78ba03b084eaa22717c14d13d0999
shibani <shibani@8thlight.com>
2018-04-13 14:19:36 -0400

    Start building World.

---
+++ lib/world.rb
@@ -0,0 +1,52 @@
+class World
+
+  attr_reader :cells
+  ALIVE = 1
+  DEAD = 0
+
+  def initialize()
+    @cells = Array.new(16, DEAD)
+  end
+
+  def size
+    @cells.size
+  end
+
+  def alive?(cell)
+    @cells[cell] == ALIVE
+  end
+
+  def dead?(cell)
+    !alive?(cell)
+  end
+
+  def toggle_life(cell)
+    current_state = @cells[cell]
+    next_state = current_state == ALIVE ? DEAD : ALIVE
+    @cells[cell] = next_state
+  end
+
+  def neighbors(cell)
+    top = cell - 4
+    top_right = cell - 3
+    right = cell + 1
+    bottom_right = cell + 5
+    bottom = cell + 4
+    bottom_left = cell + 3
+    left = cell - 1
+    top_left = cell - 5
+    neighbors = []
+
+    neighbors << @cells[top_left]
+    neighbors << @cells[top]
+    neighbors << @cells[top_right]
+    neighbors << @cells[left]
+    neighbors << @cells[right]
+    neighbors << @cells[bottom_right]
+    neighbors << @cells[bottom]
+    neighbors << @cells[bottom_left]
+
+    neighbors
+  end
+
+end
---
+++ spec/world_spec.rb
@@ -0,0 +1,38 @@
+require 'minitest/autorun'
+require './lib/world'
+
+class WorldTest < Minitest::Test
+
+  def test_that_it_has_a_10x10_world
+    world = World.new
+    assert_equal 16, world.size
+  end
+
+  def test_cells_start_dead
+    world = World.new
+    assert world.dead?(0)
+  end
+
+  def test_cells_can_toggle_aliveness
+    world = World.new
+
+    world.toggle_life(10)
+    assert world.alive?(10)
+
+    world.toggle_life(10)
+    assert world.dead?(10)
+  end
+
+  def test_cell_returns_neighbors
+    world = World.new
+    world.toggle_life(5)
+    world.toggle_life(8)
+    world.toggle_life(10)
+    world.toggle_life(13)
+
+    neighbors = world.neighbors(9)
+
+    assert_equal [0, 1, 0, 1, 1, 0, 1, 0], neighbors
+  end
+
+end
